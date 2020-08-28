edee06d8014e5059c8a0a1d3d14fa5f34757660f
Nanxin Qin <nanxinqin@hotmail.com>
2020-08-28 10:00:26 -0700

    upload app

---
+++ app/src/main/java/com/nanxinqin/age_in_minutes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nanxinqin.age_in_minutes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
